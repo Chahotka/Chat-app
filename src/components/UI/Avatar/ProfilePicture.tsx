6e6cc1d1406380fd2622fe63378e68785390b791
Arslan Rasulov <magomrak@yandex.ru>
2023-10-10 17:42:00 +0200

    session storage

--- src/components/UI/Avatar/ProfilePicture.tsx
+++ src/components/UI/Avatar/ProfilePicture.tsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import cl from './profile-picture.module.css'
 import clAuth from '../../../styles/auth.module.css'
 import Avatar from 'react-avatar-edit'
 import Button from '../Button/Button'
 import { useProfileContext } from '../../../context/ProfileContext'
+import { UserDataInterface } from '../../../interfaces/UserDataInterface'
 
 
 interface Props {
@@ -28,6 +29,10 @@ const ProfilePicture: React.FC<Props> = ({setChangePfp}) => {
   const onSave = () => {
     setPfp(image)
     setChangePfp(false)
+
+    const userData = JSON.parse(sessionStorage['user data'])
+
+    sessionStorage.setItem('user data', JSON.stringify({...userData, profilePicture: image}))
   }
 
 
@@ -48,6 +53,7 @@ const ProfilePicture: React.FC<Props> = ({setChangePfp}) => {
         height={300}
         onCrop={onCrop}
         onClose={onClose}
+        labelStyle={{color: '#fff', width: '100%', display: 'block'}}
       />
       {
         preview && <Button text='save' action={onSave}/>
--- src/components/UI/Form/Form.tsx
+++ src/components/UI/Form/Form.tsx
@@ -1,20 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import cl from './form.module.css'
 import Button from '../Button/Button'
+import { useProfileContext } from '../../../context/ProfileContext'
+import { UserDataInterface } from '../../../interfaces/UserDataInterface'
+import { useAuthContext } from '../../../context/AuthContext'
 
 const Form: React.FC = () => {
-  const [name, setName] = useState('')
+  const [nickname, setNickname] = useState('')
+
+  const { setAuth } = useAuthContext()
+  const { name, setName } = useProfileContext()
 
   const onSend = (e: React.PointerEvent) => {
     e.preventDefault()
+    if (nickname.length < 2 || nickname.length > 20) {
+      return
+    }
+
+    setAuth(true)
+    setName(nickname)
   }
+
+  useEffect(() => {
+    if (name) {
+      const userData: UserDataInterface = JSON.parse(sessionStorage['user data'])
+
+      sessionStorage.setItem('user data', JSON.stringify({...userData, name: name}))
+    }
+  }, [name])
+
 
   return (
     <form className={cl.form}>
       <input 
         className={cl.input} 
-        value={name}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+        value={nickname}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setNickname(e.target.value)}
         type="text"  
         placeholder='Enter your nickname'
       />
