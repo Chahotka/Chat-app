bf90cdf3756acfbc2e96e352b78f93a727f881ab
Arslan Rasulov <magomrak@yandex.ru>
2023-11-09 16:55:03 +0200

    Avatar scaling

--- src/components/UI/Avatar/Avatar.tsx
+++ src/components/UI/Avatar/Avatar.tsx
@@ -4,17 +4,27 @@ import pen from '../../../images/pen.svg'
 import { useAppSelector } from '../../../app/hooks'
 
 interface Props {
+  avtSize: 'large' | 'medium' | 'small'
   changeAvatar: boolean
   setChangeAvatar: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Avatar: React.FC<Props> = ({changeAvatar, setChangeAvatar}) => {
+const Avatar: React.FC<Props> = ({avtSize, changeAvatar, setChangeAvatar}) => {
   const avatar = useAppSelector(state => state.user.avatar)
+  const sizeStyle = [cl.avatarContainer]
+
+  if (avtSize === 'large') {
+    sizeStyle.push(cl.large)
+  } else if (avtSize === 'medium') {
+    sizeStyle.push(cl.medium)
+  } else if (avtSize === 'small') {
+    sizeStyle.push(cl.small)
+  }
 
   return (
     <div className={cl.avatarBox}>
       <div 
-        className={cl.avatarContainer}
+        className={sizeStyle.join(' ')}
         onClick={() => setChangeAvatar(true)}
       >
         { avatar && <img className={cl.avatar} src={avatar} /> }
