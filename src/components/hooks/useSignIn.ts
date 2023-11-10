9b3398f2f502f1c376ac9f55ba7b51701ed9df36
Arslan Rasulov <magomrak@yandex.ru>
2023-11-10 17:23:10 +0200

    check profile for verification

--- src/components/hooks/useSignIn.ts
+++ src/components/hooks/useSignIn.ts
@@ -20,22 +20,26 @@ export const useSignIn = (
   const signIn = async (e: React.MouseEvent) => {
     e.preventDefault()
     if (!user.isValid) {
+      setError('Enter info')
       return
     }
 
     const response: DocumentData | false = await dbHandler.getUser(user.email)
     
     if (response) {
-      if (hashed(user.password, response.salt).hash === response.password) {
-        dispatch(authorize())
-        dispatch(setUserProfile(response))
-      } else {
-        setError('invalid email or password')
+      if (hashed(user.password, response.salt).hash !== response.password) {
+        setError('Invalid email or password')
         return
+      } else if (!response.verificated) {
+        setError('User is not verificated')
+        return
+      } else {
+        dispatch(setUserProfile(response))
+        dispatch(authorize())
       }
     } else {
       setError('Invalid email or password')
-      return
+      return  
     }
 
     navigate('/')
