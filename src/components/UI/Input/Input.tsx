a33541d64ae5b1612de0a7a0d188c721c369b386
Arslan Rasulov <magomrak@yandex.ru>
2023-12-05 18:21:02 +0200

    input upd

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -2,32 +2,29 @@ import React, { useState } from 'react'
 import cl from './input.module.css'
 
 interface Props {
-  type: 'nick' | 'email' | 'password',
+  type: 'name' | 'email' | 'password',
   value: string
   setValue: React.Dispatch<React.SetStateAction<string>>
-  style?: string 
 }
 
-const Input: React.FC<Props> = ({ type, value, setValue, style }) => {
+const Input: React.FC<Props> = ({ type, value, setValue }) => {
   const [styles, setStyles] = useState([cl.inputBox])
 
   return (
     <div className={styles.join(' ')}>
-      { !style &&
-        <label 
-          htmlFor={type}
-          className={value ? [cl.label, cl.hide].join(' ') : cl.label}
-        >
-          Enter your { type }
-        </label>
-      }
+      <label 
+        htmlFor={type}
+        className={value ? [cl.label, cl.hide].join(' ') : cl.label}
+      >
+        Enter your { type }
+      </label>
       <input 
         id={type}
         value={value}
-        onBlur={() => !style && setStyles([cl.inputBox])}
-        onFocus={() => !style && setStyles([cl.inputBox, cl.active])}
+        onBlur={() => setStyles([cl.inputBox])}
+        onFocus={() => setStyles([cl.inputBox, cl.active])}
         onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
-        className={style ? cl.inputFilter : cl.input} 
+        className={cl.input} 
         type={type}
       />
     </div>
