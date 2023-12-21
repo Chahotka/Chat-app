c61a0b79c21df5295030862ae0234053b42448af
Arslan Rasulov <magomrak@yandex.ru>
2023-12-21 19:59:17 +0200

    UI improvement, user can now see other users infomation

--- src/components/Rooms/Search.tsx
+++ src/components/Rooms/Search.tsx
@@ -1,15 +1,67 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import cl from '../../styles/search.module.css'
 import Button from '../UI/Button/Button'
+import { useFetch } from '../hooks/useFetch'
 
-const Search: React.FC = () => {
+interface Props {
+  setActive: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+const Search: React.FC<Props> = ({setActive}) => {
+  const bg = useRef<HTMLDivElement>(null)
+  const [error, setError] = useState('')
   const [searchText, setSearchText] = useState('')
-  const [searchType, setSearchType] = useState('email')
+  const [searchBy, setSearchBy] = useState('email')
+  const fetchOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({searchText, searchBy})
+  }
+  const { fetching, loading } = useFetch(async () => {
+    console.log('searching...')
+    setError('')
+
+    if (searchText.length < 4) {
+      setError(`User ${searchBy} too short`)
+      return
+    }
+
+    const response = await fetch('http://localhost:5000/search-user', fetchOptions)
+    const data = await response.json()
+
+    console.log(data)
+  }, setError)
+
+  const onSearch = async () => {
+    console.log(`Searching user ${searchText}by ${searchBy}`)
+    await fetching()
+    console.log('done')
+  }
+
+  const bgClickHandler = (e: React.MouseEvent) => {
+    if (e.target !== bg.current) {
+      return
+    }
+
+    setSearchText('')
+    setActive(false)
+  }
   
   return (
-    <div className={cl.searchBg}>
+    <div 
+      ref={bg}
+      className={cl.searchBg}
+      onClick={(e: React.MouseEvent) => bgClickHandler(e)}
+    >
       <div className={cl.search}>
+        <div 
+          className={cl.close}
+          onClick={() => setActive(false)}
+        ></div>
         <h1 className={cl.title}>Add user</h1>
+        <p className={cl.error}>{ error }</p>
         <div className={cl.inputBox}>
           <input 
             type="text" 
@@ -23,7 +75,7 @@ const Search: React.FC = () => {
               name="searh" 
               id="search-by"
               className={cl.select} 
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setSearchType(e.target.value)}
+              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setSearchBy(e.target.value)}
             >
               <option className={cl.option} value="email">Email</option>
               <option className={cl.option} value="id">ID</option>
@@ -39,7 +91,7 @@ const Search: React.FC = () => {
             fontSize: '10px',
             zIndex: '1'
           }}
-          action={() => console.log('searching...')} 
+          action={onSearch} 
         />
       </div>
     </div>
