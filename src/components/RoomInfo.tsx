c38a15a1126def3dd78c96470e754bbe54193b31
Arslan Rasulov <magomrak@yandex.ru>
2023-12-25 17:13:36 +0200

    Future component for info about room

---
+++ src/components/RoomInfo.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import cl from '../styles/room-info.module.css'
+
+const RoomInfo: React.FC = () => {
+
+  return (
+    <div className={cl.info}>
+
+    </div>
+  )
+}
+
+export default RoomInfo
