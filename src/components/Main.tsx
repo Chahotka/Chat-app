7e27605a511330a894f08183dd37c49be72ecb8c
Arslan Rasulov <magomrak@yandex.ru>
2023-12-28 17:00:07 +0200

    Fix: Outlet cmp now renders in main cmp

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,16 @@
 import React from 'react'
 import cl from '../styles/main.module.css'
 import Rooms from './Rooms'
-import Chat from './Main/Chat'
-import RoomInfo from './RoomInfo'
+import { Outlet } from 'react-router-dom'
 import { useAppSelector } from '../app/hooks'
 
 const Main: React.FC = () => {
   const user = useAppSelector(state => state.user)
 
-
   return (
     <div className={cl.main}>
-      <Rooms users={user.rooms} />
-      <Chat />
-      <RoomInfo />
+      <Rooms />
+      <Outlet />
     </div>
   )
 }
