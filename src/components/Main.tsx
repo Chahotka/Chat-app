bdb9679c1199037b136515039f5b666c7445b931
Arslan Rasulov <magomrak@yandex.ru>
2024-01-17 16:40:43 +0200

    Checking if user is in active session, if not redirecting

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import cl from '../styles/main.module.css'
 import Rooms from './Rooms'
-import { Outlet } from 'react-router-dom'
-import { useAppSelector } from '../app/hooks'
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAppDispatch, useAppSelector } from '../app/hooks'
+import { setProfile } from '../features/user/UserSlice'
+import { authorize } from '../features/auth/AuthSlice'
 
 const Main: React.FC = () => {
-  const user = useAppSelector(state => state.user)
+  const dispatch = useAppDispatch()
+  const authorized = useAppSelector(state => state.auth.authorized)
+
+  useEffect(() => {
+    const userData = sessionStorage.getItem('user')
+
+    if (typeof userData === 'string') {
+      const user = JSON.parse(userData)
+
+      dispatch(authorize())
+      dispatch(setProfile(user))
+    }
+  }, [])
+
+  if (!authorized) {
+    return <Navigate to='/auth' />
+  }
 
   return (
     <div className={cl.main}>
