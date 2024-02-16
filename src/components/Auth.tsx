1cf9f981c5778b6d447795abda85ec16ee8a9dc6
Arslan Rasulov <magomrak@yandex.ru>
2024-02-16 17:47:09 +0200

    auth

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -2,15 +2,15 @@ import React, { useEffect } from 'react'
 import cl from '../styles/auth.module.css'
 import Button from './UI/Button/Button'
 import { useNavigate } from 'react-router-dom'
-import { useAppSelector } from '../app/hooks'
 
 
 const Auth: React.FC = () => {
   const navigate = useNavigate()
-  const authorized = useAppSelector(state => state.auth.authorized)
   
   useEffect(() => {
-    if (authorized) {
+    const storedUser = sessionStorage.getItem('user') 
+
+    if (typeof storedUser === 'string') {
       navigate('/')
     }
   }, [])
