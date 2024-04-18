24c9bfd3e9049199dc93adc6311d825b4c25347a
Arslan Rasulov <magomrak@yandex.ru>
2024-04-18 17:23:07 +0200

    Shows up when receving call

---
+++ src/components/Rooms/Chat/Call/Receiving.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import cl from '../../../../styles/call-room.module.css'
+import phone from '../../../../images/phone.svg'
+import callBg from '../../../../images/Chahotka.mp4'
+import { CallPermission } from '../../../hooks/useWebRTC'
+
+interface Props {
+  caller: CallPermission | undefined
+  startCall: () => void
+  stopCall: () => void
+}
+
+const Receiving: React.FC<Props> = ({caller, stopCall, startCall}) => {
+  return (
+    <div className={cl.receiving}>
+      <video className={cl.receivingBg} autoPlay playsInline loop src={callBg}></video>
+      <div className={cl.caller}>
+        <p className={cl.callerName}>{caller?.callerName}MUTMUT calling...</p>
+      </div>
+      <div className={cl.receivingOptions}>
+        <div onClick={startCall} className={cl.receivingOption}>
+          <img src={phone} alt="answer call" />
+        </div>
+        <div onClick={stopCall} className={[cl.receivingOption, cl.red].join(' ')}>
+          <img src={phone} alt="end call" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Receiving
